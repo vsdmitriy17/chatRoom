a673947ebae4a49716075a43c41b5125d3437ab2
Dmitriy Voskoboynikov <voskoboynikov010684@gmail.com>
2023-03-11 21:16:27 +0200

    feature/connection to server

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import './App.css';
-
 import React, { useEffect } from 'react';
 
 import socketIO from 'socket.io-client';
 
 import styles from './App.module.css';
-
-// import { Join } from "./components/Join";
 
 const serverHttp = "http://localhost:5000"; // хост сервера
 
@@ -16,7 +12,7 @@ function App() {
     }, []);
     return (
         <div className={styles.container}>
-            <h1 className={styles.title}>HELLO!!!</h1>
+            <h1 className={styles.title}>CONNECTION SUCCESS!!!</h1>
         </div>
     );
 }
