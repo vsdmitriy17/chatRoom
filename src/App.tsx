10865a3d3d6fc865911ed1bde4743fe184ebeccb
Dmitriy Voskoboynikov <voskoboynikov010684@gmail.com>
2023-03-11 20:48:06 +0200

    app/styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import './App.css';
 
+import React, { useEffect } from 'react';
+
+import socketIO from 'socket.io-client';
+
+import styles from './App.module.css';
+
 // import { Join } from "./components/Join";
 
+const serverHttp = "http://localhost:5000"; // хост сервера
+
 function App() {
+    useEffect(() => {
+        socketIO(serverHttp); // Конектимся к нашему серверу
+    }, []);
     return (
-        <div>
-            <h1>HELLO!!!</h1>
-            {/* <Join /> */}
+        <div className={styles.container}>
+            <h1 className={styles.title}>HELLO!!!</h1>
         </div>
     );
 }
